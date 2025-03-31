1d626265cad0818f3eff8e9a484327151b32ca07
Alan <dsyou2008@gmail.com>
2025-03-31 11:19:25 -0500

    bottom

--- src/models/NormalPathCard.java
+++ src/models/NormalPathCard.java
@@ -1,5 +1,9 @@
 package models;
 
-public class NormalPathCard {
-    
+public class NormalPathCard extends PathCard{
+    public NormalPathCard(String c1, String c2, int points){
+        setPoints(points);
+        setCities(c1, c2);
+        //set Images
+    }
 }
