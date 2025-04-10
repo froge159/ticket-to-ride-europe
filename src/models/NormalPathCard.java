1102dd35953226900e022c60ae26aaafcbb72e6e
Alan <dsyou2008@gmail.com>
2025-04-10 10:40:01 -0500

    gamePanel setup

--- src/models/NormalPathCard.java
+++ src/models/NormalPathCard.java
@@ -2,6 +2,7 @@ package models;
 
 public class NormalPathCard extends PathCard{
     public NormalPathCard(String c1, String c2, int points){
+        super();
         setPoints(points);
         setCities(c1, c2);
         //set Images
